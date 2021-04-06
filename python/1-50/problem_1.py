6af546e11bbcfa7e38c37b991a72aafdaae0245b
Klevberg <augustkl@hotmail.com>
2021-04-06 17:26:23 +0200

    Solved problem 2

--- python/1-50/problem_1.py
+++ python/1-50/problem_1.py
@@ -1,4 +1,4 @@
-"""
+'''
 Project Euler - Problem 1
 
 Name: Multiples of 3 and 5
@@ -7,7 +7,7 @@ Description:
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
-"""
+'''
 
 def sum_of_multiples(num_below, *nums_multiple):
 
