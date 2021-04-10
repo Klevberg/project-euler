b0cb2889f63eb49ae8c0762bbf733566834a6720
Klevberg <augustkl@hotmail.com>
2021-04-10 12:42:39 +0200

    Solved problem 22

---
+++ python/022.py
@@ -0,0 +1,16 @@
+# Name:     Names scores
+# Source:   https://projecteuler.net/problem=22
+
+
+def name_scores(filename):
+
+    with open(filename) as f:
+
+        names = sorted(f.read().strip('"').split('","'))
+        
+        values = [sum(map(lambda x: ord(x) - 64, name)) for name in names]
+        
+        return sum((i + 1)*value for i, value in enumerate(values))
+
+
+print(name_scores("./python/resources/p022_names.txt"))
