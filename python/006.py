8b467d9fe7e050c140c94e090fe9a97dae42769c
Klevberg <augustkl@hotmail.com>
2021-04-11 18:30:37 +0200

    Shortened solution for problem 6

--- python/006.py
+++ python/006.py
@@ -1,19 +1,10 @@
 # Name:     Sum square difference
 # Source:   https://projecteuler.net/problem=6
 
-def sum_of_square(num):
-    
-    return sum(n**2 for n in range(1, num + 1))
+
+def sum_square_diff(num):
+
+    return sum(range(1, num + 1))**2 - sum(n**2 for n in range(1, num + 1))
 
 
-def square_of_sum(num):
-    
-    return sum(range(1, num + 1))**2
-
-
-def difference(num):
-
-    return square_of_sum(num) - sum_of_square(num)
-
-
-print(difference(100))
+print(sum_square_diff(100))
