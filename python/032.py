79f61532327acea27e04ee963306b80887407146
Klevberg <augustkl@hotmail.com>
2021-04-11 17:03:51 +0200

    Shortened problem 32

--- python/032.py
+++ python/032.py
@@ -2,14 +2,4 @@
 # Source:   https://projecteuler.net/problem=32
 
 
-from itertools import product
-
-
-def pandigital_products_sum():
-
-    pd_products = set(p for x, y in product(range(2, 100), range(111, 5_000)) if '0' not in (nums := f'{x}{y}{(p := x*y)}') and len(set(nums)) == len(nums) == 9)
-    
-    return sum(pd_products)
-
-
-print(pandigital_products_sum())
+print(sum(set(p for x in range(2, 100) for y in range(111, 5_000) if '0' not in (nums := f'{x}{y}{(p := x*y)}') and len(set(nums)) == len(nums) == 9)))
