759ff5dd1a334813b7fb76bbe57872ac3e9537ab
Klevberg <augustkl@hotmail.com>
2021-04-12 01:40:20 +0200

    Shortened solution for problem 29

--- python/029.py
+++ python/029.py
@@ -2,12 +2,9 @@
 # Source:   https://projecteuler.net/problem=29
 
 
-from itertools import product
-
-
 def distinct_terms(a_max, b_max):
 
-    return len(set(a**b for a, b in product(range(2, a_max + 1), range(2, b_max + 1))))
+    return len(set(a**b for a in range(2, a_max + 1) for b in range(2, b_max + 1)))
 
 
 print(distinct_terms(100, 100))
