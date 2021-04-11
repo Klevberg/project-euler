6de29447d617fd50c1f27b109889488eab15d213
Klevberg <augustkl@hotmail.com>
2021-04-11 18:02:29 +0200

    Shortened the solution of problem 1

--- python/001.py
+++ python/001.py
@@ -1,19 +1,10 @@
 # Name:     Multiples of 3 and 5
 # Source:   https://projecteuler.net/problem=1
 
+
 def sum_of_multiples(num_below, *nums_multiple):
 
-    multiples = []
-
-    for num in nums_multiple:
-        acc = num # accumulator
-
-        while acc < num_below:
-            if acc not in multiples:
-                multiples.append(acc)
-            acc += num
-
-    return sum(multiples)
+    return sum(set(n for n in range(num_below) for num in nums_multiple if n % num == 0))
 
 
 print(sum_of_multiples(1000, 3, 5))
