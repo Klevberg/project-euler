52f308fe7786633c04efaeee8ce67f7372ba7821
Klevberg <augustkl@hotmail.com>
2021-04-12 01:27:20 +0200

    Shortened solution for problem 4

--- python/004.py
+++ python/004.py
@@ -1,21 +1,12 @@
 # Name:     Largest palindrome product
 # Source:   https://projecteuler.net/problem=4
 
-from itertools import product
-
-
-def is_palindrome(num):
-
-    return str(num).endswith(str(num)[:len(str(num))//2][::-1])
-
 
 def largest_palindrome_product(digits):
 
-    max_ = int("9"*digits)
-    min_ = int("1" + "0"*(digits - 1))
-    range_ = range(max_, min_ - 1, -1)
+    rng = range(int("9"*digits), 10**(digits - 1), -1)
 
-    return max(x*y for x, y in product(range_, range_) if is_palindrome(x*y))
+    return max(p for x in rng for y in rng if (num_s := str(p := x*y)).endswith(num_s[:len(num_s)//2][::-1]))
 
 
 print(largest_palindrome_product(3))
