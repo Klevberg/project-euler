20f858ef7d561577296f5477bbdbd361f309768b
Klevberg <augustkl@hotmail.com>
2021-04-12 20:45:57 +0200

    Shortened solution for problem 32 in Python

--- python/032.py
+++ python/032.py
@@ -2,4 +2,4 @@
 # Source:   https://projecteuler.net/problem=32
 
 
-print(sum(set(p for x in range(2, 100) for y in range(111, 5_000) if '0' not in (nums := f'{x}{y}{(p := x*y)}') and len(set(nums)) == len(nums) == 9)))
+print(sum({p for x in range(2, 100) for y in range(111, 5_000) if '0' not in (nums := f'{x}{y}{(p := x*y)}') and len(set(nums)) == len(nums) == 9}))
