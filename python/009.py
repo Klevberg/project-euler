18a47e6e551ae7f59bffa9babb4f24f44d459492
Klevberg <augustkl@hotmail.com>
2021-04-12 20:14:17 +0200

    Shortened solution for problem 9 in Python

--- python/009.py
+++ python/009.py
@@ -2,13 +2,7 @@
 # Source:   https://projecteuler.net/problem=9
 
 
-def get_product_of_triplet(sum_):
-    
-    for a in range(1, sum_):
-        for b in range(a + 1, sum_):
-            c = sum_ - a - b
-
-            if a**2 + b**2 == c**2: return a*b*c
+product_of_triplet = lambda sum_: next(a*b*c for a in range(1, sum_) for b in range(a + 1, sum_) if a**2 + b**2 == (c := sum_ - a - b)**2)
 
 
-print(get_product_of_triplet(1000))
+print(product_of_triplet(1000))
