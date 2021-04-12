5924af2ebea00e2f3211bd8277cbbec29e576bfd
Klevberg <augustkl@hotmail.com>
2021-04-12 22:05:47 +0200

    Shortened solution for problem 30 in Python

--- python/030.py
+++ python/030.py
@@ -2,9 +2,4 @@
 # Source:   https://projecteuler.net/problem=30
 
 
-def fifth_powers_sum():
-    
-    return sum(n for n in range(2, 200_000) if n == sum(map(lambda x: int(x)**5, str(n))))
-
-
-print(fifth_powers_sum())
+print(sum(n for n in range(2, 200_000) if n == sum(map(lambda x: int(x)**5, str(n)))))
