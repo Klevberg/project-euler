8c6c1d8341da60371c9e9244cfbf35991041f695
Klevberg <augustkl@hotmail.com>
2021-04-12 21:53:38 +0200

    Shortened solution for problem 6 in Python

--- python/006.py
+++ python/006.py
@@ -2,9 +2,7 @@
 # Source:   https://projecteuler.net/problem=6
 
 
-def sum_square_diff(num):
-
-    return sum(range(1, num + 1))**2 - sum(n**2 for n in range(1, num + 1))
+sum_square_diff = lambda num: sum(range(1, num + 1))**2 - sum(n**2 for n in range(1, num + 1))
 
 
 print(sum_square_diff(100))
