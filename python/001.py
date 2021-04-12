a111c16601711b4e9172a77a3e1a8dac1de130d7
Klevberg <augustkl@hotmail.com>
2021-04-12 20:15:22 +0200

    Shortened solution for problem 1 in Python

--- python/001.py
+++ python/001.py
@@ -2,9 +2,7 @@
 # Source:   https://projecteuler.net/problem=1
 
 
-def sum_of_multiples(num_below, *nums_multiple):
-
-    return sum(set(n for n in range(num_below) for num in nums_multiple if n % num == 0))
+sum_of_multiples = lambda num_below, *nums_multiple: sum(set(n for n in range(num_below) for num in nums_multiple if n % num == 0))
 
 
 print(sum_of_multiples(1000, 3, 5))
